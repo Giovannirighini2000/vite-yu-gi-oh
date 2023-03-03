94a09197f9aebbfff0554e272736f9ee4833df87
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-03-03 16:41:50 +0100

    feat added stamp img and p

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex item-aling gap">
+        <figure>
+            <img class="img-logo" src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg"
+                alt="">
+        </figure>
+        <h1>
+            Yu-Gi-Oh Api
+        </h1>
+
+    </div>
+</template>
+<style lang="scss">
+.img-logo {
+
+    width: 150px;
+}
+
+.gap {
+    gap: 10px;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="back-1">
+        <!-- <select></select> -->
+        <div class="back-2">
+            <ul class="container">
+                <div class="black flex item-aling">
+                    <p class="white">found 39 cards</p>
+                </div>
+                <li v-for="card in cards" :key="card.id">
+                    <img :src="card.card_images[0].image_url" alt="">
+                    <p>{{ card.name }}</p>
+                    <p>
+                        {{ card.archetype }}
+                    </p>
+
+
+
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            cards: []
+        }
+
+
+    },
+    methods: {
+        feathCards() {
+            console.log('cards')
+            axios
+                .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                .then((res) => {
+                    console.log(res.data.data)
+                    this.cards = res.data.data
+                })
+
+
+        }
+    },
+    created() {
+        this.feathCards()
+    }
+
+}
+
+</script>
+<style lang="scss" scoped>
+.back-1 {
+    background-color: #d48f38;
+    height: 100vh;
+
+}
+
+.back-2 {
+    background-color: white;
+}
+
+.black {
+    background-color: #212529;
+    height: 50px;
+}
+
+.white {
+    color: white;
+    padding-left: 10px;
+    font-weight: bold;
+}
+</style>
